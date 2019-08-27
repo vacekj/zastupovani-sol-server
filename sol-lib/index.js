04588ea52af8b1428696769d6452e6b95e3612ab
Josef Vacek <vacekj@outlook.com>
2019-08-27 15:52:33 +0200

    tests(add): all

--- sol-lib/index.js
+++ sol-lib/index.js
@@ -17,20 +17,32 @@ function launchBrowser() {
  *
  * @param {Browser} browser
  * @param {string} date
- * @returns {Promise<void>}
+ * @returns {Promise<{browser: Browser, error: Error|null, fetchDate?: string}>}
  */
 async function getSuplovani(browser, date) {
-	const page = (await browser.pages())[0];
-	await page.goto("https://aplikace.skolaonline.cz/SOL/App/Rozvrh/KSU016_SuplovaniVypis.aspx");
-	await page.waitFor("#ctl00_main_DBDatum_wdcDatum_input");
-	await page.evaluate((dateText) => {
-		document.querySelector("#ctl00_main_DBDatum_wdcDatum_input").value = dateText;
-	}, date);
-	await page.click("#ctl00_main_rbStudent");
-	await page.click("[name='ctl00$main$btnZobraz']");
+	try {
+		const page = (await browser.pages())[0];
+		await page.goto("https://aplikace.skolaonline.cz/SOL/App/Rozvrh/KSU016_SuplovaniVypis.aspx");
+		await page.waitFor("#ctl00_main_DBDatum_wdcDatum_input");
+		await page.evaluate(`
+		document.querySelector("#ctl00_main_DBDatum_wdcDatum_input").value = "${date}";`);
+		await page.click("#ctl00_main_DBDatum_wdcDatum_input"); /* Needed for input to defocus and update its text*/
+		await page.click("#ctl00_main_rbStudent");
+		await page.click("[name='ctl00$main$btnZobraz']");
+		await page.waitForNavigation();
+		if ((await page.$("#ctl00_main_DBDatum_CVDateValidator")) != null) {
+			return { browser, error: new DateNotFoundError() };
+		} else {
+			await page.waitForSelector("#ctl00_main_lblVypisDatum");
+			const fetchDate = await page.evaluate(`
+		document.querySelector("#ctl00_main_lblVypisDatum").innerHTML;
+		`);
 
-	const fetchDate = (await page.waitFor("#ctl00_main_lblVypisDatum")).innerText;
-	return fetchDate;
+			return { browser, error: null, fetchDate };
+		}
+	} catch (e) {
+		return { browser, error: e };
+	}
 }
 
 /**
@@ -38,7 +50,7 @@ async function getSuplovani(browser, date) {
  * @param {Browser} browser
  * @param username
  * @param password
- * @returns {Promise<Error|{browser: *, page: *}>}
+ * @returns {Promise<{browser: Browser, error: Error|null, username?: string}>}
  */
 async function login(browser, username, password) {
 	const page = (await browser.pages())[0];
@@ -46,13 +58,24 @@ async function login(browser, username, password) {
 	await page.type("#JmenoUzivatele", username);
 	await page.type("#HesloUzivatele", password);
 	await page.click("#dnn_ctr994_SOLLogin_btnODeslat");
+	/* TODO: handle failed login. search for selector #dnn_ctr994_SOLLogin_lblChybaPrihlaseni */
 	try {
-		await page.waitForSelector(".user");
-		console.debug("Logged in successfully");
+		await page.waitForNavigation();
+		await page.waitForSelector(".user", {
+			timeout: 2 * 1000
+		});
+		const username =
+			await page.evaluate(`
+		document.querySelector(".username").innerHTML;
+		`);
+		return { browser, error: null, username };
 	} catch (error) {
-		return new Error("Login failed");
+		return { browser, error, };
 	}
-	return { browser, };
 }
 
-module.exports = { login, getSuplovani, launchBrowser };
+class DateNotFoundError extends Error {
+
+}
+
+module.exports = { login, getSuplovani, launchBrowser, DateNotFoundError };
--- sol-lib/index.test.js
+++ sol-lib/index.test.js
@@ -1,5 +1,5 @@
 const sollib = require("./index");
-const { Browser, Page } = require("@types/puppeteer");
+/* const { Browser, Page } = require("@types/puppeteer"); */
 
 /**
  * @var {Browser} browser
@@ -12,9 +12,40 @@ beforeEach(() => {
 		.then(browserInstance => browser = browserInstance);
 });
 
-test("Logs in", () => {
-	return sollib.login(browser, "gytool_externisti_Vacek", "Prvni_prihlaseni_45");
+test("Logs in", async () => {
+	const result = await sollib.login(browser, "gytool_externisti_Vacek", "Prvni_prihlaseni_45");
+	expect(result.error)
+		.toEqual(null);
+	expect(typeof result.username)
+		.toBe("string");
+	expect(result.username.length)
+		.toBeGreaterThan(0);
 });
+
+test("Fails to log in with bogus credentials", async () => {
+	const result = await sollib.login(browser, "aaaaaaaaa", "bbbbbbbb");
+	expect(result.error)
+		.not
+		.toBe(null);
+}, 10 * 1000);
+
+test("Gets Suplovani", async () => {
+	const login = await sollib.login(browser, "gytool_externisti_Vacek", "Prvni_prihlaseni_45");
+	const suplovani = await sollib.getSuplovani(login.browser, "4.9.2019");
+	expect(suplovani.error)
+		.toEqual(null);
+	expect(typeof suplovani.fetchDate)
+		.toBe("string");
+	expect(suplovani.fetchDate.length)
+		.toBeGreaterThan(0);
+}, 20 * 1000);
+
+test("Doesn't get suplovani of nonexistent date", async () => {
+	const login = await sollib.login(browser, "gytool_externisti_Vacek", "Prvni_prihlaseni_45");
+	const suplovani = await sollib.getSuplovani(login.browser, "1.1.2000");
+	expect(suplovani.error)
+		.toBeInstanceOf(sollib.DateNotFoundError);
+}, 20 * 1000);
 
 afterEach(() => {
 	return browser.close();
