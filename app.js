86ef2adf2cf2ed325fe730dacb5ef99fb94a2eef
Josef Vacek <vacekj@outlook.com>
2019-09-11 20:57:18 +0200

    feat: add sentry

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const Sentry = require('@sentry/node');
 const path = require('path');
 const logger = require('morgan');
 const cors = require('cors');
@@ -8,6 +9,11 @@ const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 
 const app = express();
+
+Sentry.init({ dsn: 'https://ef8151df2c86434aadc2fab354f21ae3@sentry.io/1724676' });
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
 
 require("dotenv")
 	.config();
@@ -21,9 +27,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	next(createError(404));
+});
+
+app.get('/debug-sentry', function mainHandler(req, res) {
+	throw new Error('My first Sentry error!');
 });
 
 // error handler
@@ -36,5 +49,6 @@ app.use(function (err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error');
 });
+
 
 module.exports = app;
